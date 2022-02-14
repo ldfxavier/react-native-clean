21e54d515fbaed9a4dd21d02bd3acfd168b63b0f
Lucas Xavier <ldfavier@gmail.com>
2022-02-14 11:09:09 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello world 2!</Text>
+    </View>
+  );
+}
+
+export default App;
